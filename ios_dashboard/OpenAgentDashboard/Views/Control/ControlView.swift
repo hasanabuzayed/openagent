b28b6684b5146bc1141683b97f7c6f6cd456e762
Thomas Marchand <thomas.marchand@tuta.io>
2025-12-19 15:31:36 +0000

    fix: Swift concurrency warning for ScrollOffsetPreferenceKey
    Mark defaultValue as nonisolated(unsafe) to fix Xcode Cloud build error:
    "Static property 'defaultValue' is not concurrency-safe because it is
    nonisolated global shared mutable state"

--- ios_dashboard/OpenAgentDashboard/Views/Control/ControlView.swift
+++ ios_dashboard/OpenAgentDashboard/Views/Control/ControlView.swift
@@ -688,7 +688,7 @@ struct ControlView: View {
 // MARK: - Scroll Offset Preference Key
 
 private struct ScrollOffsetPreferenceKey: PreferenceKey {
-    static var defaultValue: CGFloat = 0
+    nonisolated(unsafe) static var defaultValue: CGFloat = 0
     static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
         value = nextValue()
     }
